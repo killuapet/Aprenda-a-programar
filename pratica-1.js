eaf912878379b9f0ad6de70fd512bf86118358cc
Gabi <bscs.gabriela@gmail.com>
2022-08-25 16:07:27 -0300

    hello world

--- pratica-1.js
+++ pratica-1.js
@@ -3,3 +3,19 @@ let email = "bscs.gabriela@gmail.com"
 let profissao = "estudante"
 let idade = 18
 let viagemDosSonhos = "Japao"
+
+let nome = "Gabi"
+console.log(nome)
+
+let email = "bscs.gabriela@gmail.com"
+console.log(email)
+
+let profissao = "estudante"
+console.log(profissao)
+
+let idade = "18 anos"
+console.log(idade)
+
+let viagemDosSonhos = "Japao"
+console.log(viagemDosSonhos)
+
