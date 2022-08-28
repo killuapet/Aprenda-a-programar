9f074d8ba75331672932b000645dd2bab448d438
Gabi <bscs.gabriela@gmail.com>
2022-08-28 13:44:13 -0300

    jotaro

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+let multiplicador = 9
+let n = 9 
+let i = 1
+
+while (i <= n) {
+    let resultado = multiplicador * i
+    let textoDeExibicao = `${multiplicador} X ${i} = ${resultado}`
+    console.log (textoDeExibicao)
+    i++
+}
